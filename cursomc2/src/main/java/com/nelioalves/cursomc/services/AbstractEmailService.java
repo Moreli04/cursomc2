fabcb16456a6618c5afb4dd1084f768aa4fb181b
Moreli04 <leosmoreli@gmail.com>
2019-11-16 17:33:02 -0300

    final do spring security

--- cursomc2/src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ cursomc2/src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -5,13 +5,14 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
-	
+
 	@Value("${default.sender}")
 	private String sender;
-	
+
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
@@ -19,12 +20,26 @@ public abstract class AbstractEmailService implements EmailService {
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
+		return montarEmail(obj.getCliente().getEmail(), "Pedido confirmado! Código: " + obj.getId(), obj.toString());
+	}
+
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		return montarEmail(cliente.getEmail(), "Solicitação de nova senha", "Nova senha: " + newPass);
+	}
+
+	private SimpleMailMessage montarEmail(String email, String subject, String text) {
 		SimpleMailMessage sm = new SimpleMailMessage();
-		sm.setTo(obj.getCliente().getEmail());
+		sm.setTo(email);
 		sm.setFrom(sender);
-		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
+		sm.setSubject(subject);
 		sm.setSentDate(new Date(System.currentTimeMillis()));
-		sm.setText(obj.toString());
+		sm.setText(text);
 		return sm;
 	}
 }
