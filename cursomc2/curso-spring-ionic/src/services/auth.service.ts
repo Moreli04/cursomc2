648924b5656f190bb3abc5dbdc0d3f2a0e2efa1a
Moreli04 <leosmoreli@gmail.com>
2019-11-24 20:43:44 -0300

    Criando pagina de signup

--- cursomc2/curso-spring-ionic/src/services/auth.service.ts
+++ cursomc2/curso-spring-ionic/src/services/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
                 responseType: 'text'
             });
     }
-
+    
     refreshToken() {
         return this.http.post(
             `${API_CONFIG.baseUrl}/auth/refresh_token`,
